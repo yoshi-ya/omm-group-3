11984554e536cd50b9e220f70e172d37bb57214c
yoshi-ya <y.yacoubyousif@gmail.com>
2022-01-03 16:41:15 +0100

    simple App structure

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,47 +1,11 @@
 import './App.css';
-import {React, useEffect, useState} from 'react';
-import axios from 'axios';
+import Home from "./components/Home/Home";
 
 function App() {
-  const [meme, setMeme] = useState('');
-  const [users, setUsers] = useState('');
-  const [home, setHome] = useState('');
 
-  useEffect(() => {
-    // Runs after the first render() lifecycle
-
-    axios.get("http://localhost:5000/home")
-      .then(function (response){
-        setHome(response.data)
-      })
-  }, [])
-
-  async function postMeme(e){
-    e.preventDefault()
-
-    try{
-      await axios.post("http://localhost:5000/post_meme", {
-        meme       
-      }
-      )
-      console.log(meme);
-    }catch (error){
-        console.log(error);
-    }
-  }
-
-  return (
-    <div className="App">
-      <form onSubmit={postMeme}>
-          <input type="text" value={meme} onChange={(e) => setMeme(e.target.value)} />
-          <button type="submit">Send Meme</button>
-        </form>
-      <div>
-        <h1>Memes</h1>
-        {home}
-      </div>
-    </div>
-  );
+    return (<div className="App">
+            <Home/>
+        </div>);
 }
 
 
