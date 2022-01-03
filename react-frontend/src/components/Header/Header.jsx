ebe360c6e53a15f852ccacfd569d9ae0e6a2631b
yoshi-ya <y.yacoubyousif@gmail.com>
2022-01-03 22:30:09 +0100

    added auth btn to navbar

--- react-frontend/src/components/Header/Header.jsx
+++ react-frontend/src/components/Header/Header.jsx
@@ -2,28 +2,27 @@ import React from 'react';
 import {Route, Routes, BrowserRouter as Router, Link} from "react-router-dom";
 import Home from "../Home/Home";
 import styles from "./Header.module.css"
-import Login from "../Login/Login";
+import AuthenticationButton from "../AuthenticationButton/AuthenticationButton";
+
 
 const Header = () => {
     return (<Router>
-        <div>
-            <ul className={styles.navBar}>
-                <li className={styles.navElementLeft}>
-                    <Link className={styles.navLink} to="/">Home</Link>
-                </li>
-                <li className={styles.navElementRight}>
-                    <Link className={styles.navLink} to="/login">Login</Link>
-                </li>
-            </ul>
+            <div>
+                <ul className={styles.navBar}>
+                    <li className={styles.navElementLeft}>
+                        <Link className={styles.navLink} to="/">Home</Link>
+                    </li>
+                    <li className={styles.navElementRight}>
+                        <Link className={styles.navLink} to="/login">
+                            <AuthenticationButton/>
+                        </Link>
+                    </li>
+                </ul>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                 </Routes>
-                <Routes>
-                    <Route path="/login" element={<Login/>}/>
-                </Routes>
-        </div>
-    </Router>
-);
+            </div>
+        </Router>);
 };
 
 export default Header;
