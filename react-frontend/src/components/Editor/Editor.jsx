2b72c8a9fe94f91a6cc237cead3a31fde1825abf
yoshi-ya <y.yacoubyousif@gmail.com>
2022-01-04 16:37:15 +0100

    login requirement

--- react-frontend/src/components/Editor/Editor.jsx
+++ react-frontend/src/components/Editor/Editor.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {useAuth0} from "@auth0/auth0-react";
 
 const Editor = () => {
+    const {isAuthenticated} = useAuth0();
+
+    if (!isAuthenticated) {
+        return (
+            <div>
+                Please log in to create and upload memes.
+            </div>
+        )
+    }
+
     return (<div>
         This is the Editor-View.
-        </div>);
+    </div>);
 };
 
 export default Editor;
