3189a94e5d6fbffce49606ce21ababd86e71edcc
yoshi-ya <y.yacoubyousif@gmail.com>
2022-01-14 23:59:13 +0100

    fix #7

--- express-backend/routes.js
+++ express-backend/routes.js
@@ -67,7 +67,7 @@ module.exports = app => {
      * deletes a Meme from the database
      * URL-parameter meme: the id of the meme to be deleted
      */
-    app.delete("/deleteMeme", (req, res) => {
+    app.delete("/deleteMeme", cors(), (req, res) => {
         Meme
             .findOne({_id: req.query.meme})
             .deleteOne()
@@ -81,7 +81,7 @@ module.exports = app => {
      * deletes a Comment from the database
      * URL-parameter comment: the id of the comment to be deleted
      */
-    app.delete("/deleteComment", (req, res) => {
+    app.delete("/deleteComment", cors(), (req, res) => {
         Comment
             .findOne({comment: req.query.comment})
             .deleteOne()
@@ -93,10 +93,10 @@ module.exports = app => {
 
     /**
      * ISSUE #6: API-Meme Creation
-     * given a JSON payload with a list of meme-data, a list of URLs to respective SingleViews
-     * is returned
+     * given a JSON payload with a list of meme-data, memes are created and stored in the
+     * database, and a list of URLs to respective SingleViews is returned
      */
-    app.post("/createMeme", async (req, res) => {
+    app.post("/createMeme", cors(), async (req, res) => {
         let listOfURLs = []
         for (let memeData of req.body.data) {
             let meme = new Meme({
@@ -122,5 +122,36 @@ module.exports = app => {
         }
 
         res.send({data: listOfURLs})
+    })
+
+    /**
+     * ISSUE #7: API-Meme Retrieval
+     * given an optional set of constraints, memes are retrieved and a list of URLs to respective
+     * SingleViews is returned
+     * */
+    app.get("/retrieveMemes", cors(), (req, res) => {
+        let result = []
+        const possibleFilter = ["author", "date", "template", "text1", "text2", "text3", "text4"]
+        let dbFilter = {}
+        let sortFilter = req.query.sort && req.query.sort === "oldest" ? {date: -1} : {date: 1}
+        for (let filter of possibleFilter) {
+            if (req.query[filter]) dbFilter[filter] = req.query[filter]
+        }
+        Meme
+            .find(dbFilter)
+            .limit(req.query.limit)
+            .sort(sortFilter)
+            .then(async memes => {
+                await memes.forEach(m => {
+                    let resultData = {
+                        url: `${req.protocol}://${req.get('host')}${req.originalUrl}/singleView/${m._id}`,
+                        author: m.author,
+                        date: m.date,
+                        template: m.template
+                    }
+                    result.push(resultData)
+                })
+                res.send(result)
+            }, err => console.error(err))
     })
 }
