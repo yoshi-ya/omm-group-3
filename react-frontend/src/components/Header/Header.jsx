3edd503a6fccdb042a2335928d1fe9c3a26a02e1
yoshi-ya <y.yacoubyousif@gmail.com>
2022-01-20 23:46:23 +0100

    added test route plus formatting

--- react-frontend/src/components/Header/Header.jsx
+++ react-frontend/src/components/Header/Header.jsx
@@ -5,38 +5,38 @@ import styles from "./Header.module.css"
 import AuthenticationButton from "../AuthenticationButton/AuthenticationButton";
 import Editor from "../Editor/Editor";
 import Profile from "../Profile/Profile";
-import {Meme} from '../Meme/Meme';
-import { MemeGenerated } from '../MemeGenerated/MemeGenerated'
+import {MemeGenerated} from '../MemeGenerated/MemeGenerated'
+import TestMemes from "../TestMemes/TestMemes";
 
 
 const Header = () => {
     return (<Router>
-            <div>
-                <ul className={styles.navBar}>
-                    <li className={styles.navElementLeft}>
-                        <Link className={styles.navLink} to="/">Gallery</Link>
-                    </li>
-                    <li className={styles.navElementLeft}>
-                        <Link className={styles.navLink} to="/editor">Editor</Link>
-                    </li>
-                    <li className={styles.navElementLeft}>
-                        <Link className={styles.navLink} to="/profile">Profile</Link>
-                    </li>
-                    <li className={styles.navElementRight}>
-                        <Link className={styles.navLink} to="/login">
-                            <AuthenticationButton/>
-                        </Link>
-                    </li>
-                </ul>
-                <Routes>
-                    <Route path="/" element={<Gallery/>}/>
-                    <Route path="editor" element={<Editor/>}>
-                        <Route path="generated" element={<MemeGenerated/>}/>
-                    </Route>
-                    <Route path="/profile" element={<Profile/>}/>
-                </Routes>
-            </div>
-        </Router>);
+        <div>
+            <ul className={styles.navBar}>
+                <li className={styles.navElementLeft}>
+                    <Link className={styles.navLink} to="/">Gallery</Link>
+                </li>
+                <li className={styles.navElementLeft}>
+                    <Link className={styles.navLink} to="/editor">Editor</Link>
+                </li>
+                <li className={styles.navElementLeft}>
+                    <Link className={styles.navLink} to="/profile">Profile</Link>
+                </li>
+                <li className={styles.navElementRight}>
+                    <Link className={styles.navLink} to="/login">
+                        <AuthenticationButton/>
+                    </Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/" element={<Gallery/>}/>
+                <Route path="/editor" element={<Editor/>}/>
+                <Route path="/generated" element={<MemeGenerated/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/test" element={<TestMemes/>}/>
+            </Routes>
+        </div>
+    </Router>);
 };
 
 export default Header;
