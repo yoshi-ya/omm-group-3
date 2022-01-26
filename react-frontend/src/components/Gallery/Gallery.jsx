f51db1be3168f315a080ccaf6350bdf58cb40c4e
s-tofu <khanhphuong@outlook.de>
2022-01-26 16:47:39 +0100

    Included fetch call in overview

--- react-frontend/src/components/Gallery/Gallery.jsx
+++ react-frontend/src/components/Gallery/Gallery.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import { MemeGenerated } from '../MemeGenerated/MemeGenerated';
 import {Route, Routes, BrowserRouter as Router, Link} from "react-router-dom";
 
+
 const Gallery = () => {
-    return (<div>
-        All Memes Overview.
-        </div>);
+  const [allmemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetchMemes()
+    }, )
+  
+  const fetchMemes = async () => {
+    const resultMemes = fetch('/allMemes', {
+      method: 'get',
+      }).then(res => {
+      res.json()
+      })
+    setAllMemes(resultMemes);
+    console.log(resultMemes)
+  }
+
+
+
+    return (
+    <div>
+      <div>
+          Overview
+      </div>
+      <div>
+        Memes
+      </div>
+    </div>
+    );
 };
 
 export default Gallery;
