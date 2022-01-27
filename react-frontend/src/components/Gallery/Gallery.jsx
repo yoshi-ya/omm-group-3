ffb97c39c38147a22606ad1599dcf221b0197ff6
s-tofu <khanhphuong@outlook.de>
2022-01-27 20:37:00 +0100

    Added a swtich to single view and overview button

--- react-frontend/src/components/Gallery/Gallery.jsx
+++ react-frontend/src/components/Gallery/Gallery.jsx
@@ -38,50 +38,58 @@ const Gallery = () => {
     })
     setAllMemes(resultMemes);
     allmemes.forEach(element => {
-      element.template = window.URL.createObjectURL(element.template);
+      //element.template = window.URL.createObjectURL(element.template);
+      console.log(typeof element.template)
     });
     //setData(resultMemes[0].template);
     //setVotes(resultMemes[0].votes)
   }
 
-  const Overview = (prop) => {
+  const Overview = (props) => {
 
     const memesArray = allmemes.map((meme, i) => (
     <div className={styles.item} key={i}>
-      <img width='250px' height='250px' alt="meme" src={meme.template}/>
+      <img width='250px' height='250px' alt="meme" src={`data:image/png;base64,${meme.template}`} onClick={()=>{setPage('singleview')}}/>
       <div className={styles.iconbox}>
-        <div className={styles.iconbox}>{meme.votes}<img src={like} className={styles.icons}></img></div>
+        <div className={styles.iconbox}>{meme.votes}<img src={like} className={styles.icons} onClick={() => { handleLikeClick() }}></img></div>
         <div className={styles.iconbox}><img src={comment} className={styles.icons}></img></div>
         <div className={styles.iconbox}><img src={share} className={styles.icons}></img></div>
       </div>
     </div>
     )); // `.map()` creates/returns a new array from calling a function on every element in the array it's called on
 
+    function handleLikeClick() {
+      console.log('LIKED')
+    }
+
     return (
       <div>
       <h2>Overview</h2>
+      <button className={styles.button} onClick={()=>{setPage('singleview')}}>Switch to Single View</button>
       <div className={styles.container}>{memesArray}</div> 
       </div>
     );
   }
 
-  const SingleView = () => {
-
+  const SingleView = (props) => {
+  
     const memesArray = allmemes.map((meme, i) => (
-    <div className={styles.slide}>
+    <div className={styles.slide} key={i}>
       <img width='1000px' height='1000px' alt="meme" src={`data:image/png;base64,${meme.template}`}/>
     </div>
     )); // `.map()` creates/returns a new array from calling a function on every element in the array it's called on
     
     const slideDotsArray = allmemes.map((meme, i) => (
-      <div key={i} className={styles.dot}></div>
+        <div key={i} className={styles.dot}></div>
     ));
 
     const [index, setIndex] = useState(0)
+    const delay = 2500;
 
     return (
       <div>
       <h2>Single View</h2>
+      <button className={styles.button} onClick={()=>{setPage('overview')}}>Switch to  Overview</button>
       <div className={styles.slideshow}>
         <div 
           className={styles.slideshowSlider}
@@ -92,7 +100,9 @@ const Gallery = () => {
         <div className={styles.slideshowDots}>
           {slideDotsArray}
         </div>
-      </div> 
+      </div>
+      Votes
+      Comments 
       </div>
     );
   }
