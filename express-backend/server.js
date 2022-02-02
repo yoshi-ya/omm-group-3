a9123ef8b7c1f83bace64d71073ec30243c73339
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-02 20:59:09 +0100

    extended urlencoding

--- express-backend/server.js
+++ express-backend/server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) =>{
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger)
 require('./routes/commentRoutes.js')(app)
