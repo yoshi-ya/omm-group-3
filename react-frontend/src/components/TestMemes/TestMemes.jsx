08568df97106dc305d2a52cb9a441f17154f53d4
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-02 20:57:17 +0100

    use new upload routes and logic for upload

--- react-frontend/src/components/TestMemes/TestMemes.jsx
+++ react-frontend/src/components/TestMemes/TestMemes.jsx
@@ -14,19 +14,48 @@ const TestMemes = () => {
     const [templates, setTemplates] = useState([])
 
     useEffect(() => {
-        axios
-            .get("http://localhost:5001/allMemes")
-            .then(data => setMemes(data.data))
+        fetchData()
+            .then(data => {
+                setMemes(data[0].data)
+                setTemplates(data[1].data)
+            })
+            .catch(error => console.log(error))
+
     }, []);
 
-    useEffect(() => {
-        axios
-            .get("http://localhost:5001/allTemplates")
-            .then(data => setTemplates(data.data))
-    }, []);
+    const fetchData = async () => {
+        return await Promise.all([
+            axios.get("http://localhost:5001/allMemes"),
+            axios.get("http://localhost:5001/allTemplates")
+        ])
+    }
+
+    const upload = (event, route) => {
+        event.preventDefault()
+        const memeFormData = new FormData()
+        let image = event.target.image.files[0]
+        memeFormData.append("image", image)
+        Array.from(event.target).forEach(elem => {
+            if (elem.name && elem.name !== "image") memeFormData.append(elem.name, elem.value)
+        })
+
+        axios({
+            method: "post",
+            url: `http://localhost:5001/${route}`,
+            data: memeFormData,
+            headers: {"content-type": "multipart/form-data"}
+        })
+            .then(fetchData)
+            .then(data => {
+                setMemes(data[0].data)
+                setTemplates(data[1].data)
+            })
+            .catch(error => console.log(error))
+    }
 
 
     return (<>
+        <h3>memes</h3>
         <ul>
             {memes.map(meme => {
                 return (<li key={meme._id}>
@@ -35,7 +64,7 @@ const TestMemes = () => {
                 </li>)
             })}
         </ul>
-
+        <h3>templates</h3>
         <ul>
             {templates.map(template => {
                 return (<li key={template._id}>
@@ -46,19 +75,18 @@ const TestMemes = () => {
         </ul>
 
         <h1>Upload Memes</h1>
-        <form name="memeForm" action="http://localhost:5001/upload" method="POST"
-              encType="multipart/form-data">
-            <input type="file" name="meme" id="meme" accept="image/png, image/jpg, image/jpeg" required/>
-            <input type="text" name="text1" id="text1" placeholder="text1" required/>
-            <input type="text" name="author" id="author" placeholder="author"/>
+        <form onSubmit={e => upload(e, "addMeme")}>
+            <input type="file" name="image" accept="image/png, image/jpg, image/jpeg"
+                   required/>
+            <input type="text" name="text1" placeholder="text1" required/>
+            <input type="text" name="author" placeholder="author"/>
             <input type="submit" value="upload"/>
         </form>
         <h1>Upload templates</h1>
-        <form name="memeForm" action="http://localhost:5001/uploadTemplate" method="POST"
-              encType="multipart/form-data">
-            <input type="file" name="template" id="template" accept="image/png, image/jpg, image/jpeg" required/>
-            <input type="text" name="name" id="name" placeholder="Template Name" required/>
-            <input type="text" name="author" id="author" placeholder="Author"/>
+        <form onSubmit={e => upload(e, "addTemplate")}>
+            <input type="file" name="image" accept="image/png, image/jpg, image/jpeg" required/>
+            <input type="text" name="name" placeholder="Template Name" required/>
+            <input type="text" name="author" placeholder="Author"/>
             <input type="submit" value="upload"/>
         </form>
     </>);
