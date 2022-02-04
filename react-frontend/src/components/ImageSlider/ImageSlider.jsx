ed99ae9d8f257e6185a89139c52eb5ec9774435c
RauschkugelxD <lisagoertz95@gmx.de>
2022-02-04 16:11:44 +0100

    Updated css

--- react-frontend/src/components/ImageSlider/ImageSlider.jsx
+++ react-frontend/src/components/ImageSlider/ImageSlider.jsx
@@ -3,9 +3,10 @@ import imageSlider from './ImageSlider.module.css'
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 import {encode} from "base64-arraybuffer";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 
-const ImageSlider = ({user}) => {
+const ImageSlider = ({user, sliderText, sliderButton}) => {
 
     const [slideIndex, setSlideIndex] = useState(0)
     const [memes, setMemes] = useState([])
@@ -29,32 +30,45 @@ const ImageSlider = ({user}) => {
         setSlideIndex(index)
     }
 
-    if (memes.length > 0) return (
-        <div className={imageSlider.slider}>
-            <div><FaArrowAltCircleLeft className={imageSlider.leftArrow}/></div>
-            <div><FaArrowAltCircleRight className={imageSlider.rightArrow}/></div>
+    /* Show or not show a button inside the slider */
+    function showButton(props) {
+        if (props) {
+            return <div className={imageSlider.sliderButton}><Link to="/">Gallery</Link></div>
+        } else {
+            return ""
+        }
+    }
 
-            <div className={imageSlider.sliderInner} key={memes[slideIndex]._id}>
 
-                <div className={imageSlider.slideActive}>
-                    <img src={`data:image/png;base64,${encode(memes[slideIndex].template.data)}`} alt="Meme"
-                         className={imageSlider.imageSmall}/>
+    if (memes.length > 0) 
+        return (
+            <div className={imageSlider.slider}>
+                <div><FaArrowAltCircleLeft className={imageSlider.leftArrow}/></div>
+                <div><FaArrowAltCircleRight className={imageSlider.rightArrow}/></div>
+
+                <div className={imageSlider.sliderInner} key={memes[slideIndex]._id}>
+
+                    <div className={imageSlider.slideActive}>
+                        <img src={`data:image/png;base64,${encode(memes[slideIndex].template.data)}`} alt="Meme"
+                            className={imageSlider.imageSmall}/>
+                    </div>
+
+                    <div className={imageSlider.containerDots}>
+                        {Array.from({length: 5}).map((item, index) => (<div
+                                className={slideIndex === index + 1 ? imageSlider.dotActive : imageSlider.dot}
+                                key={index}>
+                            </div>))}
+                    </div>
+
                 </div>
-
-                <div className={imageSlider.containerDots}>
-                    {Array.from({length: 5}).map((item, index) => (<div
-                            className={slideIndex === index + 1 ? imageSlider.dotActive : imageSlider.dot}
-                            key={index}>
-                        </div>))}
-                </div>
-
             </div>
-        </div>)
-    return (
-        <div className={imageSlider.emptySlider}>
-            <div>Let's create some Memes!</div>
-        </div>
-    )
+        )
+        return (
+            <div className={imageSlider.emptySlider}>
+                <div>Let's {sliderText} some Memes!</div>
+                {showButton(sliderButton)} 
+            </div>
+        )
 };
 
 export default ImageSlider;
