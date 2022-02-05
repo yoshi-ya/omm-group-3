420164c14f44c40980dc5e961652310864275ff0
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-05 23:29:54 +0100

    editor workspace css

--- react-frontend/src/components/Editor/Editor.jsx
+++ react-frontend/src/components/Editor/Editor.jsx
@@ -1,16 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {useAuth0} from "@auth0/auth0-react";
 import styles from '../Editor/Editor.module.css';
 import {Outlet} from 'react-router-dom'
-import {encode} from "base64-arraybuffer";
 import Toolbox from "../Toolbox/Toolbox";
 
 
 const Editor = () => {
 
     const {isAuthenticated} = useAuth0();
-    const [memes, setMemes] = useState([]);
-    const [currTemplate, setcurrTemplate] = useState(null);
 
     if (!isAuthenticated) {
         return (<div>
@@ -18,34 +15,10 @@ const Editor = () => {
         </div>)
     }
 
-    const handleMeme = (_meme) => {
-        if (currTemplate == null) {
-            setcurrTemplate(_meme);
-        }
-    }
-
     return (<>
         <Toolbox />
         <div className={styles.outerContainer}>
             <div className={styles.editorContainer}>
-                <div className={styles.images}>
-                    <div className={styles.container}>
-                        {memes.map(meme => {
-                            return (<div className={styles.templates} key={meme._id}>
-                                <img className={styles.meme} width='50px' height='50px'
-                                     onClick={() => handleMeme(meme)}
-                                     src={`data:image/png;base64,${encode(meme.template.data)}`}
-                                     alt={`meme_${meme._id}`}/>
-                            </div>)
-                        })}
-                    </div>
-                    {currTemplate !== null ?
-                        <div className={styles.singleIMG} key={currTemplate._id}>
-                            <img className={styles.meme} width='400px' height='400px'
-                                 src={`data:image/png;base64,${encode(currTemplate.template.data)}`}
-                                 alt={`meme_${currTemplate._id}`}/>
-                        </div> : <></>}
-                </div>
                 <Outlet/>
             </div>
         </div>
