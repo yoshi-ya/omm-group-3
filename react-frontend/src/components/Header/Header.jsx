ae3294d4bc5c61db7fc15d5b7ef69bdbefa20451
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-05 15:11:05 +0100

    removed duplicated routed

--- react-frontend/src/components/Header/Header.jsx
+++ react-frontend/src/components/Header/Header.jsx
@@ -37,7 +37,6 @@ const Header = () => {
                 <Route path="/" element={<Gallery/>}/>
                 <Route path="/profile" element={<Profile/>}/>
                 <Route path="/view/:id" element={<SingleView/>}/>
-                <Route path="/test" element={<TestMemes/>}/>
                 <Route path="/profile/*" element={<Profile/>}/>
                 <Route path="editor" element={<Editor/>}>
                     <Route path="generated" element={<MemeGenerated/>}/>
