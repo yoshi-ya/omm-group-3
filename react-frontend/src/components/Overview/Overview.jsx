5b71b1e33a0aec765610955f9efca7a6f5165224
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-05 11:01:32 +0100

    css structure

--- react-frontend/src/components/Overview/Overview.jsx
+++ react-frontend/src/components/Overview/Overview.jsx
@@ -1,4 +1,4 @@
-import styles from "../Gallery/Gallery.module.css";
+import styles from "./Overview.module.css";
 import {encode} from "base64-arraybuffer";
 import React, { useState } from "react";
 import {useAuth0} from "@auth0/auth0-react";
