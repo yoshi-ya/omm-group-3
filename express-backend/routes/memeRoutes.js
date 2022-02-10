f99f389141c302091ebc53d4cdafce39b2d2d2f8
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-10 20:39:32 +0100

    updated delete meme route

--- express-backend/routes/memeRoutes.js
+++ express-backend/routes/memeRoutes.js
@@ -84,10 +84,11 @@ module.exports = app => {
      */
     app.delete("/deleteMeme", cors(), (req, res) => {
         Meme
-            .findOne({_id: req.query.meme})
-            .deleteOne()
+            .findOneAndDelete({_id: req.body.meme})
             .then(result => {
-                res.send(result)
+                if (result) {
+                    res.send(result)
+                }
             })
             .catch(err => console.log(err))
     })
