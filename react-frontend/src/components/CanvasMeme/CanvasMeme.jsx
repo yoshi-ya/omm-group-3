fb60c01331327d6116612adab7d155fdb8ef9f39
Iris <turba@cip.ifi.lmu.de>
2022-02-12 23:15:48 +0100

    single view chart

--- react-frontend/src/components/CanvasMeme/CanvasMeme.jsx
+++ react-frontend/src/components/CanvasMeme/CanvasMeme.jsx
@@ -1,14 +1,36 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect,useState, useRef} from 'react';
+import {Bar} from 'react-chartjs-2';
 import styles from "./CanvasMeme.module.css";
+import Chart from 'chart.js/auto'
+import axios from 'axios';
 
 
 const CanvasMeme = ({meme}) => {
 
     const canvasRef = useRef(0)
+    const [comments, setComments] = useState([]);
+   // const numOfVotes = Object.keys(meme.votes).length
+    const numOfVotes = meme.votes.length;
+
+    useEffect(() => {
+        if (meme._id) {
+            fetchComments()
+                .then(data => {
+                    setComments(data.data)
+                    console.log(comments);
+                })
+                .catch(error => console.log(error))
+        }
+    }, [meme])
+
+    const fetchComments = async () => {
+        return await axios.get(`http://localhost:5001/allComments?meme=${meme._id}`)
+    }
 
 
     useEffect(() => {
         if (meme && meme.templates && meme.templates.length > 0) {
+            console.log(meme);
             const context = canvasRef.current.getContext("2d")
             context.fillStyle = "black"
             context.fillRect(0, 0, meme.canvasWidth, meme.canvasHeight)
@@ -32,9 +54,40 @@ const CanvasMeme = ({meme}) => {
 
     if (!meme) return <div/>
 
-    return (
+return (
+        <div>
         <canvas ref={canvasRef} width={meme.canvasWidth ? meme.canvasWidth : 400} height={meme.canvasHeight ? meme.canvasHeight : 400}
-                className={styles.canvas}/>);
+                className={styles.canvas}/>
+                
+        <div className={styles.smallgraph}> 
+        <Bar
+            data={{
+            labels: ['Votes/Comments'],
+            datasets: [
+                {
+                  id: 1,
+                  label: '# of votes',
+                  data: [numOfVotes],
+                  backgroundColor: 'blue'
+                },
+                {
+                  id: 2,
+                  label: '# of comments',
+                  data: [comments.length],
+                  backgroundColor: 'red'
+                },
+              ],
+              options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+ }} 
+ 
+       
+    
+/></div> </div>
+                );
+                
 };
 
 export default CanvasMeme;
