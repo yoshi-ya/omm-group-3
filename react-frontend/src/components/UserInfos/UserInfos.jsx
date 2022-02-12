9989537f3e3d10dc36a070a67d5b56a492ffcb48
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-12 20:12:15 +0100

    css fixes

--- react-frontend/src/components/UserInfos/UserInfos.jsx
+++ react-frontend/src/components/UserInfos/UserInfos.jsx
@@ -2,49 +2,48 @@ import React, {useState, useEffect, useRef} from 'react';
 import axios from "axios";
 import {useAuth0} from '@auth0/auth0-react';
 import userInfos from "./UserInfos.module.css"
-import ImageSlider from '../ImageSlider/ImageSlider'; // For slide-show
+import ImageSlider from '../ImageSlider/ImageSlider';
 import {encode} from "base64-arraybuffer";
 import Editor from "../Editor/Editor";
-import {Link} from "react-router-dom";
-//import PopUp from '../PopUp/SharePopUp';
+
 
 const UserInfos = () => {
 
     const {user} = useAuth0();
-    const [avatar, setAvatar] = useState(null);
+    const [avatar, setAvatar] = useState("https://via.placeholder.com/200?text=no%20profile%20picture");
     const [myMemes, setMyMemes] = useState([]);
     const [otherMemes, setOtherMemes] = useState([]);
     const [allMemes, setAllMemes] = useState([]);
-    const inputAvatar= useRef(null);  
+    const inputAvatar = useRef(null);
 
     // Handle state changes of memes that the logged in user has created
     useEffect(() => {
         fetchMyMemes()
-        .then(myMemes => {
-            //console.log('Fetched data (my memes): ', myMemes)
-            setMyMemes(myMemes.data) 
-        })
+            .then(myMemes => {
+                //console.log('Fetched data (my memes): ', myMemes)
+                setMyMemes(myMemes.data)
+            })
     }, [])
-    
+
     // Handle state changes of all memes in database
     useEffect(() => {
         fetchAllMemes()
-        .then(allMemes => {
-            //console.log('Fetched data (all Memes): ', allMemes)
-            setAllMemes(allMemes.data)  
-        })
-    }, [])  
+            .then(allMemes => {
+                //console.log('Fetched data (all Memes): ', allMemes)
+                setAllMemes(allMemes.data)
+            })
+    }, [])
 
     // Handle state changes of memes that the logged in user has liked
     useEffect(() => {
         getVotedMemes()
-        .then(result => {
-            //console.log('Result: ', result)
-            setOtherMemes(result) 
-        })  
+            .then(result => {
+                //console.log('Result: ', result)
+                setOtherMemes(result)
+            })
     }, [allMemes])
 
-    
+
     // Get all memes from server that are createb by the logged in user
     const fetchMyMemes = async () => {
         return await axios.get(`http://localhost:5001/allMemes?author=${user.name}`)
@@ -52,7 +51,7 @@ const UserInfos = () => {
 
     // Get all memes from server 
     const fetchAllMemes = async () => {
-        return await axios.get('http://localhost:5001/allMemes') 
+        return await axios.get('http://localhost:5001/allMemes')
     }
 
     // Search for memes that the logged in user has voted for
@@ -62,8 +61,8 @@ const UserInfos = () => {
         //console.log('listOfAllMemes: ', listOfAllMemes)
 
         // Loop through the votes of all memes
-        for (var i = 0; i < allMemes.length-1; i++) {
-            let listOfVotes = [...allMemes[i].votes] 
+        for (var i = 0; i < allMemes.length - 1; i++) {
+            let listOfVotes = [...allMemes[i].votes]
             //console.log('listOfVotes :', listOfVotes)
 
             // Compare the names of the votes with the logged in user
@@ -77,9 +76,9 @@ const UserInfos = () => {
 
     // Delete selected meme
     function deleteMeme(memeID) {
-        axios 
+        axios
             .delete("http://localhost:5001/deleteMeme", {data: {meme: memeID}})
-            .then(setMyMemes(myMemes.filter( (meme) => meme._id !== memeID) ))
+            .then(setMyMemes(myMemes.filter((meme) => meme._id !== memeID)))
             .catch(err => console.log(err))
     }
 
@@ -98,11 +97,6 @@ const UserInfos = () => {
             .catch(error => console.log(error))
     }
 
-    /*function showPopup() {
-        return <PopUp/>;
-        <div className={userInfos.button} onClick={showPopup}>Followers</div>
-    }*/
-
     // Select an image from the desktop
     const fileSelectedHandler = (event) => {
         console.log(event.target.files[0]);
@@ -111,44 +105,36 @@ const UserInfos = () => {
 
     const handleClick = () => {
         inputAvatar.current.focus();
-     }
+    }
 
-    return (
-        <div className={userInfos.container}>
+    return (<div className={userInfos.container}>
             <div className={userInfos.card}>
                 <div className={userInfos.imageArea}>
-                    <div className={userInfos.avatar}/>
-                    <div className={userInfos.avatarButton}>
-                        <input type='file' name="chooseAvatar" // <div className={userInfos.cameraButton} onClick={handleClick}></div>
-                            accept="image/png, image/jpg, image/jpeg" required 
-                            onChange={fileSelectedHandler} onClick={fileUploadHandler}//e => fileUploadHandler(e) ???
-                            //ref={fileInput => this.fileInput = fileInput}
-                            ref={inputAvatar}
-                            className={userInfos.cameraButton}
-                        />
-                    </div>
+                    <img src={avatar} alt="profile-pic"/>
                 </div>
-                    
-                <h3 style={{textAlign: "center"}}>User:</h3>
-                <p className={{textAlign: "center"}}> {user.name} </p>
-
+                <div className={userInfos.welcome}>
+                    <span style={{textAlign: "center"}}>Welcome back,</span>
+                    <h2 style={{textAlign: "center"}}> {user.name} </h2>
+                </div>
             </div>
 
             <div className={userInfos.verticalBox}>
 
                 <div className={userInfos.card}>
                     <h3 className={userInfos.cardTitle}>My created memes</h3>
-                    <ImageSlider memes={myMemes} sliderText={"Let's create a meme!"} sliderButton={'Editor'} deleteMeme={deleteMeme} author={true} />
+                    <ImageSlider memes={myMemes} sliderText={"Let's create a meme!"}
+                                 sliderButton={'Editor'} deleteMeme={deleteMeme} author={true}/>
                 </div>
-                
+
                 <div className={userInfos.card}>
                     <h3 className={userInfos.cardTitle}>Memes I liked or commented</h3>
-                    <ImageSlider memes={otherMemes} sliderText={"Let's search for some funny memes!"} sliderButton={'Gallery'} author={false}/>
+                    <ImageSlider memes={otherMemes}
+                                 sliderText={"Let's search for some funny memes!"}
+                                 sliderButton={'Gallery'} author={false}/>
                 </div>
-                
+
             </div>
-        </div>
-    );
+        </div>);
 }
 
 export default UserInfos;
