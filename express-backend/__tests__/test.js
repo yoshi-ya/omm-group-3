618b74e1533064a4408e81c4b0265213738e3f28
yoshi-ya <y.yacoubyousif@gmail.com>
2022-02-13 19:41:27 +0100

    fixed deletion test

--- express-backend/__tests__/test.js
+++ express-backend/__tests__/test.js
@@ -2,14 +2,14 @@ const request = require("supertest");
 const server = 'http://localhost:5001'
 
 // ID of the meme used to test
-const testId= '6202a0ea093088997f95b710'
+const testId = '6202a0ea093088997f95b710'
 const testUser = 'Test User'
 const testComment = 'This is a test comment'
 
 describe('Test if we get any memes at all', () => {
     it('GET /allMemes result length is greater than 0', () => {
-        request(server).get('/allMemes').end((error,res) => {
-            if(error) {
+        request(server).get('/allMemes').end((error, res) => {
+            if (error) {
                 throw error
             } else {
                 expect(res.body.length).toBeGreaterThan(0);
@@ -20,8 +20,8 @@ describe('Test if we get any memes at all', () => {
 
 describe('Test getting one single meme with specific ID', () => {
     it(`GET /fetchMeme with test ID ${testId}`, () => {
-        request(server).get(`/fetchMeme?id=${testId}`).end((error,res) => {
-            if(error) {
+        request(server).get(`/fetchMeme?id=${testId}`).end((error, res) => {
+            if (error) {
                 throw error
             } else {
                 expect(res.body._id).toBe(testId);
@@ -31,44 +31,44 @@ describe('Test getting one single meme with specific ID', () => {
 });
 
 describe('Test adding a like to Meme', () => {
-    it(`POST /addLike to Meme with test ID ${testId}`, async() => {
+    it(`POST /addLike to Meme with test ID ${testId}`, async () => {
         let oldVotes
         let numberOfVotes
 
         await request(server).get(`/fetchMeme?id=${testId}`).then((result) => {
-                oldVotes = result.body.votes
-                numberOfVotes = result.body.votes.length
-                request(server).post(`/addLike`, {id:testId, votes: [...oldVotes, testUser]}).end((error,res) =>{
-                    if(error) {
-                        throw error
-                    } else {
-                        if(res.body.votes) {
-                            expect(res.body.votes.length).toBe(numberOfVotes+1)
-                        }
+            oldVotes = result.body.votes
+            numberOfVotes = result.body.votes.length
+            request(server).post(`/addLike`, {
+                id: testId, votes: [...oldVotes, testUser]
+            }).end((error, res) => {
+                if (error) {
+                    throw error
+                } else {
+                    if (res.body.votes) {
+                        expect(res.body.votes.length).toBe(numberOfVotes + 1)
                     }
-                })
+                }
+            })
         })
     })
 });
 
 
 describe('Test adding a comment to Meme', () => {
-    it(`POST /addComment to Meme with test ID ${testId}`, async() => {
+    it(`POST /addComment to Meme with test ID ${testId}`, async () => {
         let commentData = {
-            'meme': testId,
-            'author': testUser,
-            'content': testComment
+            'meme': testId, 'author': testUser, 'content': testComment
         }
 
-        let numberOfComments
-        await request(server).get(`/allComments?meme=${testId}`).then(async (result) =>{
+        let numberOfComments = 0
+        await request(server).get(`/allComments?meme=${testId}`).then(async (result) => {
             numberOfComments = result.body.length
-            await request(server).post(`/addComment`).send(commentData).then(() =>{
-                request(server).get(`/allComments?meme=${testId}`).end((error, res) =>{
-                    if(error){
+            await request(server).post(`/addComment`).send(commentData).then(() => {
+                request(server).get(`/allComments?meme=${testId}`).end((error, res) => {
+                    if (error) {
                         throw error
                     } else {
-                        expect(res.body.length).toBe(numberOfComments+1)
+                        expect(res.body.length).toBe(numberOfComments + 1)
                     }
                 })
             })
@@ -78,20 +78,20 @@ describe('Test adding a comment to Meme', () => {
 
 describe('Test deleting a comment of Meme', () => {
     it('DELETE /deleteComment deletion of added test comment', async () => {
-        await request(server).get(`/allComments?meme=${testId}`).then(async (result) =>{
-            numberOfComments = result.body.length
-            await request(server).delete('/deleteComment', {comment:testComment, meme:testId, author:testUser}).then(() =>{
-                request(server).get(`/allComments?meme=${testId}`).end((error, res) =>{
-                    if(error){
+        await request(server).get(`/allComments?meme=${testId}`).then(async (result) => {
+            let numberOfComments = result.body.length
+            await request(server).delete('/deleteComment').send({
+                comment: testComment
+            }).then(() => {
+                request(server).get(`/allComments?meme=${testId}`).end((error, res) => {
+                    if (error) {
                         throw error
                     } else {
-                        expect(res.body.length).toBe(numberOfComments-1)
+                        expect(res.body.length).toBe(numberOfComments - 1)
                     }
                 })
             })
         })
     })
 })
-
-
 
